47cf270107e669157a5fab87183cf71c3b1fd81b
Teletekst vici <74468041+teletekstvici@users.noreply.github.com>
2020-12-10 23:22:15 +0100

    Disable social posting

--- .github/scripts/publish-image.js
+++ .github/scripts/publish-image.js
@@ -6,7 +6,7 @@ const postToSocials = require('./post-to-socials');
     const image = await border('./.github/assets/latest.png');
     const date = (new Date()).toLocaleString('sl-SI', { weekday: 'long', month: 'long', day: 'numeric' });
     await png(image); // Build website
-    await postToSocials(`${date.charAt(0).toUpperCase()}${date.slice(1)}`, await watermark(image));
+    // await postToSocials(`${date.charAt(0).toUpperCase()}${date.slice(1)}`, await watermark(image));
   } catch (error) {
     console.error(error);
   }
